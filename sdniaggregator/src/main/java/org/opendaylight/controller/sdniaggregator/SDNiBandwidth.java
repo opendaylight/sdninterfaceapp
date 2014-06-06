fa26ba467292dcd99a42cbd7f20625b2555dbc0b
Ed Warnicke <eaw@cisco.com>
2014-06-06 12:56:11 +0530

    "first commit"
    Change-Id: Iab6c494cace34417735034580020b6ee6d753f31
    Signed-off-by: rafatjahan <rafat.jahan@tcs.com>
    Signed-off-by: Ed Warnicke <eaw@cisco.com>

---
+++ sdniaggregator/src/main/java/org/opendaylight/controller/sdniaggregator/SDNiBandwidth.java
@@ -0,0 +1,42 @@
+package org.opendaylight.controller.sdniaggregator;
+
+import java.util.Set;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+
+@XmlRootElement
+@XmlAccessorType(XmlAccessType.NONE)
+public class SDNiBandwidth {
+		@XmlElement
+	    Set<String> bandwidth;
+		@XmlElement
+		String name;
+		
+		public SDNiBandwidth() {
+			// TODO Auto-generated constructor stub
+		}
+		public SDNiBandwidth(Set<String> bandwidth, String name) {
+            this.bandwidth = bandwidth;
+            this.name = name;
+	    }
+	
+	     public Set<String> getBandwidth() {
+	            return bandwidth;
+	    }
+	
+	    public void setBandwidth(Set<String> bandwidth) {
+	    		this.bandwidth = bandwidth;
+	    }
+	    
+	    public String getName() {
+	        return name;
+	    }
+	
+	    public void setName(String name) {
+	    		this.name = name;
+	    }
+}
