c92dd0935cbd278ce2622e8aa9b3adc93a10c1e6
rafatjahan <rafat.jahan@tcs.com>
2014-06-11 11:46:46 +0530

    "Updated POM"
    Change-Id: Ibc68e0f8a480eec9458431a01a8d1042084f0aef
    Signed-off-by: rafatjahan <rafat.jahan@tcs.com>

---
+++ sdni/src/test/java/org/opendaylight/controller/sdni/northbound/SdniNorthboundTest.java
@@ -0,0 +1,30 @@
+/*
+ * Copyright (c) 2014 Cisco Systems, Inc. and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+package org.opendaylight.controller.sdni.northbound;
+
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.opendaylight.controller.sal.core.Node;
+import org.opendaylight.controller.sal.core.NodeConnector;
+import org.opendaylight.controller.sal.core.Property;
+import org.opendaylight.controller.sal.utils.NodeConnectorCreator;
+import org.opendaylight.controller.sal.utils.NodeCreator;
+
+public class SdniNorthboundTest {
+
+        @Test
+        public void testNodes() {
+                
+        }
+}
