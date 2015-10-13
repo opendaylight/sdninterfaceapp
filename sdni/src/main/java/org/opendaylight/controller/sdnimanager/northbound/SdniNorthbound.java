0b2f5ce547a30634f2ebccaa7588ebac6e76c051
Swetha <swetha.s8@tcs.com>
2015-10-13 18:39:11 +0530

    Resolved Bug 4457
    Change-Id: I16d8f37fd19744e18abbfca51ffaacf0164068f4
    Signed-off-by: Swetha <swetha.s8@tcs.com>

--- sdni/src/main/java/org/opendaylight/controller/sdnimanager/northbound/SdniNorthbound.java
+++ sdni/src/main/java/org/opendaylight/controller/sdnimanager/northbound/SdniNorthbound.java
@@ -8,23 +8,21 @@
 
 package org.opendaylight.controller.sdnimanager.northbound;
 
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.SecurityContext;
-import javax.ws.rs.PathParam;
 
 import org.codehaus.enunciate.jaxrs.ResponseCode;
 import org.codehaus.enunciate.jaxrs.StatusCodes;
-import org.opendaylight.controller.northbound.commons.RestMessages;
-import org.opendaylight.controller.northbound.commons.exception.ResourceNotFoundException;
-import org.opendaylight.controller.northbound.commons.exception.ServiceUnavailableException;
-import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev151006.node.connector.statistics.and.port.number.map.NodeConnectorStatisticsAndPortNumberMap;
 import org.opendaylight.sdniaggregator.OpendaylightSdniManager;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.port.statistics.rev151006.node.connector.statistics.and.port.number.map.NodeConnectorStatisticsAndPortNumberMap;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -51,11 +49,15 @@ public class SdniNorthbound {
     private static final int CLUSTER_CONFLICT = 409;
     private static final int SERVICES_UNAVAILABLE = 503;
     private static final String CONTAINER = "Container ";
-    private static final String NOT_AUTHORIZED_MESSAGE = "User is not authorized to perform this operation on container ";
+    private static final String NOT_AUTHORIZED_MESSAGE = 
+        new String("User is not authorized to perform this operation on container ");
 
-    OpendaylightSdniManager sdniManager;
-    List<NodeConnectorStatisticsAndPortNumberMap> list = new ArrayList<NodeConnectorStatisticsAndPortNumberMap>();
-
+    OpendaylightSdniManager sdniManager ;
+    List<NodeConnectorStatisticsAndPortNumberMap> list = 
+        new ArrayList<NodeConnectorStatisticsAndPortNumberMap>();
+    /**
+     * setting SecurityContext
+     */
     @Context
     public void setSecurityContext(SecurityContext context) {
         if (context != null && context.getUserPrincipal() != null) {
@@ -67,56 +69,70 @@ public class SdniNorthbound {
         return username;
     }
 
-
-  /**
-     * Retrieve the NodeConnectorStatisticsAndPortNumberMap Data like node, port, receive packets etc.,
-     * from SDNiAggregator
-     * @return NodeConnectorStatisticsAndPortNumberMap Example: Request URL:
-     *         http://localhost:8080/controller/nb/v2/sdni/default/qos
-     *         Response Body in JSON:
-     *      [{"node":"00:00:00:00:00:00:00:04","port":"0","receivePackets":0,"transmitPackets":0,"collisionCount":0,"receiveFrameError":0,
-     *      "receiveOverRunError":0,"receiveCrcError":0},
-     *      {"node":"00:00:00:00:00:00:00:04","port":"1","receivePackets":61,"transmitPackets":63,"collisionCount":0,"receiveFrameError":0,
-     *      "receiveOverRunError":0,"receiveCrcError":0},
-     *      {"node":"00:00:00:00:00:00:00:04","port":"2","receivePackets":47,"transmitPackets":47,"collisionCount":0,"receiveFrameError":0,
-     *      "receiveOverRunError":0,"receiveCrcError":0},
-     *      {"node":"00:00:00:00:00:00:00:04","port":"3","receivePackets":62,"transmitPackets":64,"collisionCount":0,"receiveFrameError":0,
-     *      "receiveOverRunError":0,"receiveCrcError":0},
-     *      {"node":"00:00:00:00:00:00:00:06","port":"4","receivePackets":70,"transmitPackets":72,"collisionCount":0,"receiveFrameError":0,
-     *      "receiveOverRunError":0,"receiveCrcError":0},
-     *      {"node":"00:00:00:00:00:00:00:06","port":"0","receivePackets":0,"transmitPackets":0,"collisionCount":0,"receiveFrameError":0,
-     *      "receiveOverRunError":0,"receiveCrcError":0},
-     *      {"node":"00:00:00:00:00:00:00:06","port":"1","receivePackets":27,"transmitPackets":290,"collisionCount":0,
-     *      "receiveFrameError":0,"receiveOverRunError":0,"receiveCrcError":0},
-     *      {"node":"00:00:00:00:00:00:00:06","port":"2","receivePackets":23,"transmitPackets":285,"collisionCount":0,
-     *      "receiveFrameError":0,"receiveOverRunError":0,"receiveCrcError":0},
-     *      {"node":"00:00:00:00:00:00:00:06","port":"3","receivePackets":61,"transmitPackets":55,"collisionCount":0,"receiveFrameError":0,
-     *      "receiveOverRunError":0,"receiveCrcError":0},
-     *      {"node":"00:00:00:00:00:00:00:05","port":"0","receivePackets":0,"transmitPackets":0,"collisionCount":0,"receiveFrameError":0,
-     *      "receiveOverRunError":0,"receiveCrcError":0},
-     *      "node":"00:00:00:00:00:00:00:05","port":"1","receivePackets":51,"transmitPackets":51,"collisionCount":0,"receiveFrameError":0,
-     *      "receiveOverRunError":0,"receiveCrcError":0},
-     *      {"node":"00:00:00:00:00:00:00:05","port":"2","receivePackets":52,"transmitPackets":54,"collisionCount":0,"receiveFrameError":0,
-     *      "receiveOverRunError":0,"receiveCrcError":0}]
-  **/
+    /**
+     * Retrieve the Network Capabilities QOS Data like node, port, receive
+     * packets etc., from SDNiAggregator
+     * 
+     *    @return NetworkCapabilitiesQOS Example: Request URL:
+     *     http://localhost:8080/controller/nb/v2/sdni/default/qos Response
+     *     Body in JSON:
+     *     [{"node":"00:00:00:00:00:00:00:04","port":"0","receivePackets":0,
+     *     "transmitPackets":0,"collisionCount":0,"receiveFrameError":0,
+     *     "receiveOverRunError":0,"receiveCrcError":0},
+     *     {"node":"00:00:00:00:00:00:00:04","port":"1","receivePackets":61,
+     *     "transmitPackets":63,"collisionCount":0,"receiveFrameError":0,
+     *     "receiveOverRunError":0,"receiveCrcError":0},
+     *     {"node":"00:00:00:00:00:00:00:04","port":"2","receivePackets":47,
+     *     "transmitPackets":47,"collisionCount":0,"receiveFrameError":0,
+     *     "receiveOverRunError":0,"receiveCrcError":0},
+     *     {"node":"00:00:00:00:00:00:00:04","port":"3","receivePackets":62,
+     *     "transmitPackets":64,"collisionCount":0,"receiveFrameError":0,
+     *     "receiveOverRunError":0,"receiveCrcError":0},
+     *     {"node":"00:00:00:00:00:00:00:06","port":"4","receivePackets":70,
+     *     "transmitPackets":72,"collisionCount":0,"receiveFrameError":0,
+     *     "receiveOverRunError":0,"receiveCrcError":0},
+     *     {"node":"00:00:00:00:00:00:00:06","port":"0","receivePackets":0,
+     *     "transmitPackets":0,"collisionCount":0,"receiveFrameError":0,
+     *     "receiveOverRunError":0,"receiveCrcError":0},
+     *     {"node":"00:00:00:00:00:00:00:06","port":"1","receivePackets":27,
+     *     "transmitPackets":290,"collisionCount":0,
+     *     "receiveFrameError":0,"receiveOverRunError":0,"receiveCrcError":0
+     *     },
+     *     {"node":"00:00:00:00:00:00:00:06","port":"2","receivePackets":23,
+     *     "transmitPackets":285,"collisionCount":0,
+     *     "receiveFrameError":0,"receiveOverRunError":0,"receiveCrcError":0
+     *     },
+     *     {"node":"00:00:00:00:00:00:00:06","port":"3","receivePackets":61,
+     *     "transmitPackets":55,"collisionCount":0,"receiveFrameError":0,
+     *     "receiveOverRunError":0,"receiveCrcError":0},
+     *     {"node":"00:00:00:00:00:00:00:05","port":"0","receivePackets":0,
+     *     "transmitPackets":0,"collisionCount":0,"receiveFrameError":0,
+     *     "receiveOverRunError":0,"receiveCrcError":0},
+     *     "node":"00:00:00:00:00:00:00:05","port":"1","receivePackets":51,
+     *     "transmitPackets":51,"collisionCount":0,"receiveFrameError":0,
+     *     "receiveOverRunError":0,"receiveCrcError":0},
+     *     {"node":"00:00:00:00:00:00:00:05","port":"2","receivePackets":52,
+     *     "transmitPackets":54,"collisionCount":0,"receiveFrameError":0,
+     *     "receiveOverRunError":0,"receiveCrcError":0}]
+     **/
 
     @Path("/{containerName}/qos")
     @GET
     @Produces({ MediaType.APPLICATION_JSON })
     @StatusCodes({ @ResponseCode(code = CONTAINER_NOT_FOUND, condition = "The Container Name was not found") })
-    public List<NodeConnectorStatisticsAndPortNumberMap> getQosDetails(){
+    public List<NodeConnectorStatisticsAndPortNumberMap> getQosDetails() {
 
-    LOGGER.info("after populated NetworkCapabilitiesQOS data");
-    try {
-       list = sdniManager.getNodeConnectorStatistics();
-    }
-    catch (Exception e) {
-    }
+        LOGGER.info("after populated NetworkCapabilitiesQOS data");
+        try {
+            list = sdniManager.getNodeConnectorStatistics();
+        } catch (Exception e) {
+            LOGGER.error("Unable to retrieve Node Connector Statistics ");
+        }
 
-    LOGGER.info("list property"+ list);
-    if(list == null){
-       throw new ServiceUnavailableException("SDNi QoS service "+ RestMessages.SERVICEUNAVAILABLE.toString());
+        LOGGER.info("list property" + list);
+        if (list == null) {
+            throw new ServiceUnavailableException("SDNi QoS Service is not available. Could be down for maintanence");
+        }
+        return list;
     }
-    return list;
-  }
 }
