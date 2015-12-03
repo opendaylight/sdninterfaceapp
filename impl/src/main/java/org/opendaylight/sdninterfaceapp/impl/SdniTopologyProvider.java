575ad1e3299bb1a26f34bec0aa4f9f99f2cb03cc
shahid8110 <shahid.b@tcs.com>
2015-12-03 22:50:17 +0530

    ODL-SDNi MD-SAL Full Migration
    Change-Id: I28e2baa225a596550ebb1e328df5c8e1cdae9920
    Signed-off-by: shahid8110 <shahid.b@tcs.com>

---
+++ impl/src/main/java/org/opendaylight/sdninterfaceapp/impl/SdniTopologyProvider.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2015 Tata Consultancy Services and others.  All rights reserved.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v1.0 which accompanies this distribution,
+ * and is available at http://www.eclipse.org/legal/epl-v10.html
+ */
+
+package org.opendaylight.sdninterfaceapp.impl;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Collections;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
+import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
+import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
+import org.opendaylight.controller.sal.binding.api.data.DataProviderService;
+import org.opendaylight.controller.md.sal.binding.api.DataBroker;
+import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.sdninterfaceapp.topology.msg.rev151006.OpendaylightSdniTopologyMsgService;
+import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class SdniTopologyProvider implements BindingAwareProvider, AutoCloseable {
+
+    private final static Logger LOG = LoggerFactory.getLogger(SdniTopologyProvider.class);
+    private DataBroker dataBroker = null;
+    private RpcRegistration<OpendaylightSdniTopologyMsgService> sdniTopologyServiceRpc; 
+
+    @Override
+    public void onSessionInitiated(ProviderContext session) {
+    	LOG.info("SdniTopology Plugin Started");
+        SdniTopologyMsgServiceImpl sdniTopologyMsgServiceImpl = new SdniTopologyMsgServiceImpl();
+        sdniTopologyServiceRpc = session.addRpcImplementation(OpendaylightSdniTopologyMsgService.class,  sdniTopologyMsgServiceImpl);
+        dataBroker = session.getSALService(DataBroker.class);   
+        sdniTopologyMsgServiceImpl.setBroker(dataBroker);
+    }
+
+    @Override
+    public void close() throws Exception {
+        LOG.info("SdniTopologyProvider Closed");
+        if (sdniTopologyServiceRpc != null) {
+            sdniTopologyServiceRpc.close();
+        }
+    }
+}
