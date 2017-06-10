f125c82126d9c154eb411b26ae21823e12324f21
Sirisha <sirisha.sangam@tcs.com>
2017-06-09 22:50:28 -0400

    Migrate to odlparent 1.8.0-Carbon:-Fixing sdninterfaceapp build failure
    Per request of odlparent project we are downgrading all Nitrogen
    projects to use the released odlparent 1.8.0-Carbon to allow for the
    odlparent project to start performing semver style releases.
    
    Change-Id: I1e3401b7196791bcd0cfcc8acb0087fdd8285fb0
    Jira: RELENG-159
    RT: 41406
    Signed-off-by: Thanh Ha <thanh.ha@linuxfoundation.org>
    Signed-off-by: sirishasangam <sirisha.sangam@tcs.com>

--- impl/src/main/java/org/opendaylight/sdninterfaceapp/impl/SdniTopologyProvider.java
+++ impl/src/main/java/org/opendaylight/sdninterfaceapp/impl/SdniTopologyProvider.java
@@ -14,7 +14,6 @@ import java.util.Collections;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.ProviderContext;
 import org.opendaylight.controller.sal.binding.api.BindingAwareBroker.RpcRegistration;
 import org.opendaylight.controller.sal.binding.api.BindingAwareProvider;
-import org.opendaylight.controller.sal.binding.api.data.DataProviderService;
 import org.opendaylight.controller.md.sal.binding.api.DataBroker;
 import org.opendaylight.yang.gen.v1.urn.opendaylight.params.xml.ns.yang.sdninterfaceapp.topology.msg.rev151006.OpendaylightSdniTopologyMsgService;
 import org.opendaylight.yang.gen.v1.urn.tbd.params.xml.ns.yang.network.topology.rev131021.TopologyId;
